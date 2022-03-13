caefc3f901e648af88c619825f63ca3e29c50b11
Mooteus <moteus10@gmail.com>
2022-03-13 14:22:04 -0300

    feat: add average function

--- src/average.js
+++ src/average.js
@@ -1,5 +1,4 @@
 /*
-
   A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
   Caso a função receba algum valor não númerico ou um array vazio,
   o valor undefined deve ser retornado.
@@ -13,6 +12,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let total = 0;
+
+  if (array.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof array[i] !== 'number') {
+      return undefined;
+    } 
+    total += array[i];
+  }
+
+  return Math.round(total / array.length);
+};
 
 module.exports = average;
