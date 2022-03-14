c3933e648303bcfdb4a8d8fece6b859df4200dca
Mooteus <moteus10@gmail.com>
2022-03-14 18:45:01 -0300

    feat: add function createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nameTxt) => {
+  
+  const student = {
+    name: nameTxt,
+    feedback: () => 'Eita pessoa boa!' };
+
+  return student;
+};
 
 module.exports = createStudent;
